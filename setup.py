c366c8ebd98ddea2cf17714161fb524fc59b5f77
adaalrf <martin.anq@gmail.com>
2024-05-27 11:51:18 +0200

    fix /setup.py, /pywal/wallpaper.py

--- setup.py
+++ setup.py
@@ -1,39 +1,35 @@
-"""wal - setup.py"""
-import sys
-import setuptools
+from setuptools import setup, find_packages
 
-try:
-    import pywal
-except ImportError:
-    print("error: pywal requires Python 3.5 or greater.")
-    sys.exit(1)
-
-LONG_DESC = open('README.md').read()
-VERSION = pywal.__version__
-DOWNLOAD = "https://github.com/dylanaraps/pywal/archive/%s.tar.gz" % VERSION
-
-setuptools.setup(
+setup(
     name="pywal",
-    version=VERSION,
-    author="Dylan Araps",
-    author_email="dylan.araps@gmail.com",
-    description="Generate and change color-schemes on the fly",
-    long_description_content_type="text/markdown",
-    long_description=LONG_DESC,
-    keywords="wal colorscheme terminal-emulators changing-colorschemes",
-    license="MIT",
+    version="3.3.0",
+    description="Generate and change color-schemes on the fly.",
     url="https://github.com/dylanaraps/pywal",
-    download_url=DOWNLOAD,
+    author="Dylan Araps",
+    author_email="dylan@dylanaraps.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "colorz",
+        "image",
+        "pillow",
+        "requests",
+        "dbus-python",  # Add dbus-python to the install requirements
+    ],
+    entry_points={
+        "console_scripts": [
+            "wal = pywal.__main__:main",
+        ],
+    },
     classifiers=[
-        "Environment :: X11 Applications",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    packages=["pywal"],
-    entry_points={"console_scripts": ["wal=pywal.__main__:main"]},
-    python_requires=">=3.5",
-    test_suite="tests",
-    include_package_data=True,
-    zip_safe=False)
+)
+
